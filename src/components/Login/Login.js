3763f63accd2779bd25e0af8ffb1dc9133f25ba0
dev_nayan <md.nayan8494@gmail.com>
2022-04-15 16:47:46 +0600

    Access the values of input in signup and signin form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+  // Declaring States 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // Event Handlers
+  const getEmail = e => {
+    const email = e.target.value;
+    setEmail(email);
+  }
+
+  const getPassword = e => {
+    const password = e.target.value;
+    setPassword(password);
+  }
+
+  const handleLogin = e => {
+    e.preventDefault();
+  }
+
+
     return (
         <div className="signup-form-wrapper">
       <h4 className='form-title'>Login</h4>
-      <form className='signup-form'>
+      <form onSubmit={handleLogin} className='signup-form'>
         <label htmlFor="email">Email</label>
         <br />
-        <input type="email" name="email" />
+        <input onBlur={getEmail} type="email" name="email" />
         <br />
         <label htmlFor="password">Password</label>
         <br />
-        <input type="password" name="password" />
+        <input onBlur={getPassword} type="password" name="password" />
         <br />
         <input className='submit-btn' type="submit" value="Login" />
       </form>
       <p style={{textAlign: 'center'}}>New to Ema-John? <Link className='login-link' to={'/signup'}>Create an account</Link></p>
       <span className='divider'>or</span>    
       <button className="google-signup-btn">
-      <i class="ri-google-fill"></i>
+      <i className="ri-google-fill"></i>
       Continue with Google
       </button>
     </div>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,30 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './SignUp.css'
 
 const SignUp = () => {
+  // Declaring States 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setComfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  // Event Handlers
+  const getEmail = e => {
+    const email = e.target.value;
+    setEmail(email);
+  }
+
+  const getPassword = e => {
+    const password = e.target.value;
+    setPassword(password);
+  }
+
+  const getConfirmPassword = e => {
+    const confirmPassword = e.target.value;
+    setComfirmPassword(confirmPassword);
+  }
+
+  const handleSignUp = e => {
+    e.preventDefault();
+  }
+
+
+
+
   return (
     <div className="signup-form-wrapper">
       <h4 className='form-title'>Sign Up</h4>
-      <form className='signup-form'>
+      <form onSubmit={handleSignUp} className='signup-form'>
         <label htmlFor="email">Email</label>
         <br />
-        <input type="email" name="email" />
+        <input onBlur={getEmail} type="email" name="email" />
         <br />
         <label htmlFor="password">Password</label>
         <br />
-        <input type="password" name="password" />
+        <input onBlur={getPassword} type="password" name="password" />
         <br />
         <label htmlFor="confirm-password">Confirm Password</label>
         <br />
-        <input type="password" name="comfirm-password" />
+        <input onBlur={getConfirmPassword} type="password" name="comfirm-password" />
         <br />
         <input className='submit-btn' type="submit" value="Sign Up" />
       </form>
       <p style={{textAlign: 'center'}}>Already have an account? <Link className='login-link' to={'/login'}>Login</Link></p>
       <span className='divider'>or</span>    
       <button className="google-signup-btn">
-      <i class="ri-google-fill"></i>
+      <i className="ri-google-fill"></i>
       Continue with Google
       </button>
     </div>
